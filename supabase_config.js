6def9cd29962d37573c444a4afc09fc67da524c4
920t920t <920t920t@gmail.com>
2026-02-23 10:39:22 +0900

    Update Supabase Anon Key to correct JWT format

--- supabase_config.js
+++ supabase_config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://vvarsaoioenxgaidsqiy.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_FwjeWZ_Szg1p0stMjSHjVA_XO6hOsnH";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2YXJzYW9pb2VueGdhaWRzcWl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3ODE4ODcsImV4cCI6MjA4NzM1Nzg4N30.R5SB0GKqLNkeUhytCQ-DhQRNDVKjM8w02WBJfvGN4Qs";
 
 // Supabaseクライアントの初期化用
 let supabase;
